2350a74d349088673d4fb9532f27e8a57ffdc560
rwcreator <rwcreator@6fb9d93d-3318-797c-659c-755e06b4d92d>
2014-01-25 19:04:32 +0000

    updated encoder code again

--- rtr2014/playground/SampleJaguar/src/org/usfirst/frc2421/SampleJaguar/Robot.java
+++ rtr2014/playground/SampleJaguar/src/org/usfirst/frc2421/SampleJaguar/Robot.java
@@ -76,9 +76,10 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         Scheduler.getInstance().run();
         System.out.print("Encoder value = " + Robot.motorTest.readEncoder());
-        System.out.print("Raw value = " + Robot.motorTest.readRaw());
-        System.out.print("Rate value = " + Robot.motorTest.readRate());
-        System.out.println("Drive value = " + Robot.motorTest.readDriven());
+        System.out.print(" Raw value = " + Robot.motorTest.readRaw());
+        System.out.print(" Rate value = " + Robot.motorTest.readRate());
+        System.out.print(" Drive value = " + Robot.motorTest.readDriven());
+        System.out.println("");
     }
     /**
      * This function called periodically during test mode
