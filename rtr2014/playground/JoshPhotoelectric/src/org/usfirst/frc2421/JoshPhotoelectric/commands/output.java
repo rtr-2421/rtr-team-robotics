a6591601bbb4fc7349033287e0a54e1a7216c7fb
josh.piel19@gmail.com <josh.piel19@gmail.com@6fb9d93d-3318-797c-659c-755e06b4d92d>
2014-01-30 01:14:51 +0000

    updated photosensor

--- rtr2014/playground/JoshPhotoelectric/src/org/usfirst/frc2421/JoshPhotoelectric/commands/output.java
+++ rtr2014/playground/JoshPhotoelectric/src/org/usfirst/frc2421/JoshPhotoelectric/commands/output.java
@@ -26,6 +26,7 @@ public class  output extends Command {
     // Called just before this Command runs the first time
     protected void initialize() {
     }
+    
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
         if (Robot.photosensor.getSensor() == true) {
@@ -38,6 +39,7 @@ public class  output extends Command {
     }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
+        
         return false;
     }
     // Called once after isFinished returns true
