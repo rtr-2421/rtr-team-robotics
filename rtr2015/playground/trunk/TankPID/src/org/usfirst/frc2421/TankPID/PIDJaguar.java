2267657982273296eeafa2fce7c32a516b1d076d
RTR2421Coders@gmail.com <RTR2421Coders@gmail.com@6fb9d93d-3318-797c-659c-755e06b4d92d>
2015-02-07 18:08:06 +0000

    "Tank drive code using speed PID" by Eyob.

---
+++ rtr2015/playground/trunk/TankPID/src/org/usfirst/frc2421/TankPID/PIDJaguar.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc2421.TankPID;
+
+import edu.wpi.first.wpilibj.CANJaguar;
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class PIDJaguar implements PIDOutput{
+
+	private CANJaguar motor;
+	
+	public PIDJaguar(CANJaguar jag){
+		motor = jag;
+	}
+	
+	public void pidWrite(double output){
+		double speed = motor.get() - output;
+		Math.max(speed, -1);
+		Math.min(speed, 1);
+		motor.set(speed);
+	}
+	
+}
