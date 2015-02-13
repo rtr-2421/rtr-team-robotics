8342f88335972f2b25b37cd627e394f6e43a8669
rwcreator@gmail.com <rwcreator@gmail.com@6fb9d93d-3318-797c-659c-755e06b4d92d>
2015-02-13 23:54:45 +0000

    client

--- ImageClient/src/imageclient/Client.java
+++ ImageClient/src/imageclient/Client.java
@@ -8,10 +8,8 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-	
-	
-	public static void main( String[] args)
-	{
+
+	public static void main(String[] args) {
 		int port = 3000;
 		int direction = 0;
 		Socket s = getSocket(port);
@@ -26,41 +24,34 @@ public class Client {
 			return;
 		}
 
-		while(true)
-		{
-		out.println("centered");
-		out.println("centered");
-		System.out.println("test");
-		String test = new String("");
-			try
-			{
+		while (true) {
+			out.println("centered");
+			out.println("centered");
+			System.out.println("test");
+			String test = new String("");
+			try {
 				System.out.println("2");
 				if (in.hasNextLine())
 					test = in.next();
 				System.out.println("3");
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				System.out.println("[Error] Lost connection to server");
 				break;
 			}
-			
-			if (test.equals("end"))
-			{
+
+			if (test.equals("end")) {
 				System.out.println("Finished");
 				break;
 			}
 		}
-		
-		try
-		{
+
+		try {
 			s.close();
 			in.close();
 			System.out.println("finished");
-		}
-		catch ( IOException ex)
-		{
-			System.out.println("[Error] Could not safely disconnect (Connection lost forcefully possibly");
+		} catch (IOException ex) {
+			System.out
+					.println("[Error] Could not safely disconnect (Connection lost forcefully possibly");
 		}
 	}
 
@@ -68,28 +59,22 @@ public class Client {
 		// TODO Auto-generated method stub
 		Socket s;
 		String host;
-		while(true)
-		{
-			try 
-			{
+		while (true) {
+			try {
 				InetAddress ip = InetAddress.getByName("127.0.0.1");
 				s = new Socket(ip, port);
 				return s;
-			}
-			catch (UnknownHostException e) 
-			{
+			} catch (UnknownHostException e) {
 				// TODO Auto-generated catch block
 				System.out.println("[Error] Could not find robot server");
 				continue;
-		
-			}
-			catch (IOException ex)
-			{
+
+			} catch (IOException ex) {
 				System.out.println("[Error] Generic network error");
 				continue;
 			}
-				
+
 		}
 	}
-		
+
 }
